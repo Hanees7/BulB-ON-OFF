b1e8f2ef79e915e07aec65249d98a349acf61173
Hanees7 <mohmmadhanees786@gmail.com>
2023-04-07 19:16:24 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Trun ONN Function
+
+function bulbOn(){
+    let bulbObj = document.querySelector(".bulboff")
+bulbObj.setAttribute("src", "Onn.jpg")
+}
+function bulbOff(){
+    let bulbObj = document.querySelector(".bulboff")
+bulbObj.setAttribute("src", "off.jpg")
+}
